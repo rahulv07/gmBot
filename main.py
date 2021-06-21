6f04b33881d148d2b5798f7aacbdc6dc49b1e93b
Rahul <rahulsudhan2601@gmail.com>
2021-06-21 22:17:34 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,91 @@
+# import required modules
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+def Glogin(mail_address, password):
+	# Login Page
+	driver.get(
+		'https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/&ec=GAZAAQ')
+
+	# input Gmail
+	driver.find_element_by_id("identifierId").send_keys(mail_address)
+	driver.find_element_by_id("identifierNext").click()
+	driver.implicitly_wait(10)
+
+	# input Password
+	driver.find_element_by_xpath(
+		'//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
+	driver.implicitly_wait(10)
+	driver.find_element_by_id("passwordNext").click()
+	driver.implicitly_wait(10)
+
+	# go to google home page
+	driver.get('https://google.com/')
+	driver.implicitly_wait(100)
+
+
+def turnOffMicCam():
+	# turn off Microphone
+	time.sleep(2)
+	driver.find_element_by_xpath(
+		'//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[1]/div[1]/div/div[4]/div[1]/div/div/div').click()
+	driver.implicitly_wait(3000)
+
+	# turn off camera
+	time.sleep(1)
+	driver.find_element_by_xpath(
+		'//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[1]/div[1]/div/div[4]/div[2]/div/div').click()
+	driver.implicitly_wait(3000)
+
+
+def joinNow():
+	# Join meet
+	time.sleep(5)
+	driver.implicitly_wait(2000)
+	driver.find_element_by_css_selector(
+		'div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()
+
+
+def AskToJoin():
+	# Ask to Join meet
+	time.sleep(5)
+	driver.implicitly_wait(2000)
+	driver.find_element_by_css_selector(
+		'div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()
+	# Ask to join and join now buttons have same xpaths
+
+def takeScreenshot(id):
+	screen = driver.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]')
+	screen.screenshot(f'lastClassSS/ss{id}.png')
+	time.sleep(10)
+
+# assign email id and password
+mail_address = '19u230@psgtech.ac.in'
+with open('password.txt','r') as f:
+	password = f.readline()
+
+# create chrome instamce
+opt = Options()
+opt.add_argument('--disable-blink-features=AutomationControlled')
+opt.add_argument('--start-maximized')
+opt.add_experimental_option("prefs", {
+	"profile.default_content_setting_values.media_stream_mic": 1,
+	"profile.default_content_setting_values.media_stream_camera": 1,
+	"profile.default_content_setting_values.geolocation": 0,
+	"profile.default_content_setting_values.notifications": 1
+})
+driver = webdriver.Chrome(options=opt)
+
+# login to Google account
+Glogin(mail_address, password)
+
+# go to google meet
+driver.get('https://meet.google.com/zba-wdyx-tfs')
+turnOffMicCam()
+# AskToJoin()
+joinNow()
+time.sleep(5)
+for id in range(1,10):
+	takeScreenshot(id)
